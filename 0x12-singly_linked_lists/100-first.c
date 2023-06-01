cd71946ab2d01d3c1f35a276f0f041a44f28993f
Lamiaa SAMRAOUI <lamiaasamraoui1999@gmail.com>
2023-06-01 07:57:42 -0700

    0x12. C - Singly linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void firstone(void) __attribute__ ((constructor));
+
+/**
+ *firstone - Function to be executed before main
+ */
+void firstone(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
